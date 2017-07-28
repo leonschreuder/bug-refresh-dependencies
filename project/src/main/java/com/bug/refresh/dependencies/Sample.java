06f33edfa5c7bd28d99dca9f8c888b2e29b63bca
lemo7242 <leon.moll@esolutions.de>
2017-07-28 14:07:24 +0200

    add init script + tryout changes

--- project/src/main/java/com/bug/refresh/dependencies/Sample.java
+++ project/src/main/java/com/bug/refresh/dependencies/Sample.java
@@ -2,7 +2,17 @@ package com.bug.refresh.dependencies;
 
 import java.io.PrintStream;
 
-public class Sample {
+import com.bug.refresh.dependencies.api.ISdk;
+
+public class Sample implements ISdk {
+
+    public String getSomeString() {
+        return "some string";
+    }
+
+    // public String getSomeOtherString() {
+    //     return "some other string";
+    // }
 
     public void hello(PrintStream out) {
         out.println("Hello world!");
--- sdk/src/main/java/com/bug/refresh/dependencies/ISdk.java
+++ sdk/src/main/java/com/bug/refresh/dependencies/ISdk.java
@@ -1,5 +1,6 @@
-package com.bug.refresh.dependencies;
+package com.bug.refresh.dependencies.api;
 
 public interface ISdk {
     public String getSomeString();
+    public String getSomeOtherString();
 }
